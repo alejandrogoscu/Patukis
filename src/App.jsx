0a0ca51264d002029edbe23b8be32fc4f9acf0c8
alejandrogoscu <alejandrogoscu@gmail.com>
2025-06-21 07:28:44 +0200

    organizacion carpetas proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import logo from './assets/Logo_1.svg';
+import patos from '/Users/alejandrogoscu/Desktop/theBridge/u4Frontend/patukis/patukisFrontend/src/assets/paticontructors.png';
 import './App.css';
 
 function App() {
-  return <></>;
+  return (
+    <div className="contenedor">
+      <img src={logo} />
+      <img src={patos} width="640px" />
+      <h2 className="text">¡Próximamente!</h2>
+    </div>
+  );
 }
 
 export default App;
