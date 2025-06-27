f073f9cb34bb95cf658667e87b8a84aabe3bb199
Miguel Rubio Sanfeliu <miguelrubiosanfeliu@MacBook-Air-de-Miguel.local>
2025-06-28 01:23:16 +0200

    Diseño aplicado

--- src/components/User/LoginForm/LoginForm.jsx
+++ src/components/User/LoginForm/LoginForm.jsx
@@ -1,6 +1,9 @@
 import { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import UserContext from '../../../context/UserContext/UserContext'; 
+import '../RegisterForm/registerForm.css';
+
+
 
 const LoginForm = () => {
   const initialValue = {
@@ -44,50 +47,55 @@ const LoginForm = () => {
   };
 
   return (
-    <div className="login_container">
-      <h2 className="login_title">Iniciar Sesión</h2>
-      <form className="login_form" onSubmit={handleSubmit}>
-        <label className="login_label" htmlFor="email">
-          Correo Electrónico
-          <input
-            className="login_input"
-            type="email"
-            name="email"
-            id="email"
-            value={FormData.email}
-            onChange={handleInputChange}
-            autoComplete="email"
-            required
-          />
-        </label>
+  <div className="register__container">
+    <form className="register__form" onSubmit={handleSubmit}>
+      <h2 className="register__title" style={{ alignSelf: "center" }}>
+          Iniciar Sesión
+      </h2>
 
-        <label className="login_label" htmlFor="password">
-          Contraseña
-          <input
-            className="login_input"
-            type="password"
-            name="password"
-            id="password"
-            value={FormData.password}
-            onChange={handleInputChange}
-            autoComplete="password"
-            required
-          />
-        </label>
 
-        <button className="login_button" type="submit">Iniciar Sesión</button>
-      </form>
+      <label className="register__label" htmlFor="email">
+        Correo Electrónico
+        <input
+          className="register__input"
+          type="email"
+          name="email"
+          id="email"
+          value={FormData.email}
+          onChange={handleInputChange}
+          autoComplete="email"
+          required
+        />
+      </label>
 
-      <div className="login_register_prompt">
-        <p className="login_register_text">
+      <label className="register__label" htmlFor="password">
+        Contraseña
+        <input
+          className="register__input"
+          type="password"
+          name="password"
+          id="password"
+          value={FormData.password}
+          onChange={handleInputChange}
+          autoComplete="password"
+          required
+        />
+      </label>
+
+      <button type="submit">Iniciar Sesión</button>
+
+      <div>
+        <p className="register__input">
           ¿Eres nueva/o en Patukis?{" "}
-          <button className="login_register_button" onClick={goToRegister}>
+          <button type="button" onClick={goToRegister}>
             Comenzar →
           </button>
         </p>
       </div>
-    </div>
-  );
+    </form>
+  </div>
+);
+
 };
 
 export default LoginForm;
