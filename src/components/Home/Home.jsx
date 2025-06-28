e522d0c3df00c9555e4d26ddc810a86812dc1efe
alejandrogoscu <alejandrogoscu@gmail.com>
2025-06-28 10:52:41 +0200

    Diseño principal home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,3 +1,22 @@
+import vid01 from '../../assets/videos/paintervid01.gif';
+import vid02 from '../../assets/videos/paintervid02.gif';
+import vid03 from '../../assets/videos/paintervid03.gif';
+import logo from '../../assets/icons/Logo_1.svg';
+import './home.css';
+
 export const Home = () => {
-  return <div>Hola</div>;
+  return (
+    <div className="home__container">
+      <div className="home__videocontainer">
+        <img className="home__video" src={vid01} />
+        <img className="home__video" src={vid02} />
+        <img className="home__video" src={vid03} />
+      </div>
+
+      <div className="home__logocontainer">
+        <img className="home__logo" src={logo} />
+        <h2 className="home__tile">patitos con personalidad</h2>
+      </div>
+    </div>
+  );
 };
