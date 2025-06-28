82a7f605e21d25e19f95ab9b99242d762fe8b674
alejandrogoscu <alejandrogoscu@gmail.com>
2025-06-28 17:48:06 +0200

    Home info Ok posible mejora

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,6 +2,7 @@ import vid01 from '../../assets/videos/paintervid01.gif';
 import vid02 from '../../assets/videos/paintervid02.gif';
 import vid03 from '../../assets/videos/paintervid03.gif';
 import logo from '../../assets/icons/Logo_1.svg';
+import patolanding from '../../assets/patito_landing.png';
 import './home.css';
 
 export const Home = () => {
@@ -16,6 +17,10 @@ export const Home = () => {
       <div className="home__logocontainer">
         <img className="home__logo" src={logo} />
         <h2 className="home__tile">patitos con personalidad</h2>
+      </div>
+
+      <div className="home__infocontainer">
+        <img className="home__info--img" src={patolanding} />
       </div>
     </div>
   );
