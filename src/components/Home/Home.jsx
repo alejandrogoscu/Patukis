52ac5bc02d89197cdcd81fb6da8904323fdfef9f
palomaceg <p.canalaecheverria@gmail.com>
2025-06-29 20:51:40 +0200

    home modificada para implementar react bits

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,30 +1,53 @@
-import vid01 from "../../assets/videos/paintervid01.gif";
-import vid02 from "../../assets/videos/paintervid02.gif";
-import vid03 from "../../assets/videos/paintervid03.gif";
-import logo from "../../assets/icons/Logo_1.svg";
-import patolanding from "../../assets/patito_landing.png";
+import GridMotion from "../ReactBits/GridMotion/GridMotion";
 import "./home.css";
-import CircularGallery from "../ReactBits/CircularGallery/CircularGallery";
+import logo from "../../assets/icons/Logo_1.svg";
+
+const items = [
+  "https://i.imgur.com/TQ6DOaa.png",
+  "personalidad que impacta",
+  "https://i.imgur.com/E1qVp21.png",
+  "Colores que resaltan",
+  "https://i.imgur.com/Iw7p8cZ.png",
+  "Estilo 100% custom made",
+  "https://i.imgur.com/OsztreL.png",
+  "Accesorios que molan",
+  "https://i.imgur.com/JkHBb9I.png",
+  "personalidad que impacta",
+  "https://i.imgur.com/pGSMhEm.png",
+  "Colores que resaltan",
+  "https://i.imgur.com/WdNbJZG.png",
+  "Estilo 100% custom made",
+  "https://i.imgur.com/gEK1QEE.png",
+  "Accesorios que molan",
+  "https://i.imgur.com/PDXdAWp.png",
+  "Accesorios que molan",
+  "https://i.imgur.com/TQ6DOaa.png",
+  "personalidad que impacta",
+  "https://i.imgur.com/JkHBb9I.png",
+  "Colores que resaltan",
+  "https://i.imgur.com/pGSMhEm.png",
+  "Estilo 100% custom made",
+  "https://i.imgur.com/WdNbJZG.png",
+  "Accesorios que molan",
+  "https://i.imgur.com/PDXdAWp.png",
+  "personalidad que impacta",
+  "https://i.imgur.com/pGSMhEm.png",
+  "Colores que resaltan",
+  "https://i.imgur.com/Fg0lYBY.png",
+  "Estilo 100% custom made",
+  "https://i.imgur.com/TQ6DOaa.png",
+  "Accesorios que molan",
+  "https://i.imgur.com/pGSMhEm.png",
+];
 
 export const Home = () => {
   return (
-    <div className="home__container">
-      <div className="home__videocontainer">
-        <img className="home__video1" src={vid01} />
-        <img className="home__video2" src={vid02} />
-        <img className="home__video3" src={vid03} />
+    <div className="full-view">
+      <div className="logo-container">
+        <img src={logo} className="logo-landing" alt="logo patukis" />
       </div>
-
-      <div className="home__logocontainer">
-        <img className="home__logo" src={logo} />
-        <h2 className="home__tile">patitos con personalidad</h2>
-      </div>
-      <div className="home__infocontainer">
-        <img className="home__info--img" src={patolanding} />
-      </div>
-      <div className="react-bits-container">
-        <h2>Exclusive ducks. Yours to see, not to buy</h2>
-        <CircularGallery />
+      <div className="grid-container">
+        <GridMotion items={items} gradientColor="orange" />
       </div>
     </div>
   );
