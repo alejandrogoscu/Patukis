72744dfa5f2d29a285ac0aa90b9f5a0c00495f4b
palomaceg <p.canalaecheverria@gmail.com>
2025-06-29 02:51:35 +0200

    patisorpresa

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,10 @@
-import vid01 from '../../assets/videos/paintervid01.gif';
-import vid02 from '../../assets/videos/paintervid02.gif';
-import vid03 from '../../assets/videos/paintervid03.gif';
-import logo from '../../assets/icons/Logo_1.svg';
-import patolanding from '../../assets/patito_landing.png';
-import './home.css';
+import vid01 from "../../assets/videos/paintervid01.gif";
+import vid02 from "../../assets/videos/paintervid02.gif";
+import vid03 from "../../assets/videos/paintervid03.gif";
+import logo from "../../assets/icons/Logo_1.svg";
+import patolanding from "../../assets/patito_landing.png";
+import "./home.css";
+import CircularGallery from "../ReactBits/CircularGallery/CircularGallery";
 
 export const Home = () => {
   return (
@@ -18,9 +19,12 @@ export const Home = () => {
         <img className="home__logo" src={logo} />
         <h2 className="home__tile">patitos con personalidad</h2>
       </div>
-
       <div className="home__infocontainer">
         <img className="home__info--img" src={patolanding} />
+      </div>
+      <div className="react-bits-container">
+        <h2>Exclusive ducks. Yours to see, not to buy</h2>
+        <CircularGallery />
       </div>
     </div>
   );
