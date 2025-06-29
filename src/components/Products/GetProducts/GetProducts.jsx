49710db8d0e29d04a39f5662627c8162a3237786
alejandrogoscu <alejandrogoscu@gmail.com>
2025-06-29 08:06:31 +0200

    CSS productos ok

--- src/components/Products/GetProducts/GetProducts.jsx
+++ src/components/Products/GetProducts/GetProducts.jsx
@@ -1,9 +1,9 @@
-import { useContext, useEffect } from "react";
-import { ProductContext } from "../../../context/ProductContext/ProductState";
-import "./getProducts.css";
-import { Link } from "react-router-dom";
-import { UserContext } from "../../../context/UserContext/UserState";
-import AdminProducts from "../AdminProducts/AdminProducts";
+import { useContext, useEffect } from 'react';
+import { ProductContext } from '../../../context/ProductContext/ProductState';
+import './getProducts.css';
+import { Link } from 'react-router-dom';
+import { UserContext } from '../../../context/UserContext/UserState';
+import AdminProducts from '../AdminProducts/AdminProducts';
 
 const GetProducts = () => {
   const { products, getProducts, addToCart } = useContext(ProductContext);
@@ -13,7 +13,7 @@ const GetProducts = () => {
     getProducts();
   }, []);
 
-  if (user?.role === "mamapato") {
+  if (user?.role === 'mamapato') {
     return (
       <>
         <AdminProducts />
@@ -23,35 +23,41 @@ const GetProducts = () => {
 
   return (
     <>
-      <h1>Productos</h1>
-      <div className="filters-container"></div>
-      <main className="products-crds">
-        {products.map((product) => (
-          <div className="card" key={product._id}>
-            <div className="card-prod-img-cont">
-              {product.image ? (
-                <img
-                  src={
-                    product.image.startsWith("http")
-                      ? product.image
-                      : `https://patukisapi.onrender.com/${product.image}`
-                  }
-                  alt={product.name}
-                  className="img-patitos"
-                />
-              ) : null}
+      <div className="products__container">
+        <h1 className="products__title">Nuestros Patitos</h1>
+        <div className="filters-container"></div>
+        <main className="products-crds">
+          {products.map((product) => (
+            <div className="card" key={product._id}>
+              <div className="card-prod-img-cont">
+                {product.image ? (
+                  <img
+                    src={
+                      product.image.startsWith('http')
+                        ? product.image
+                        : `https://patukisapi.onrender.com/${product.image}`
+                    }
+                    alt={product.name}
+                    className="img-patitos"
+                  />
+                ) : null}
+              </div>
+              <div className="prod-info">
+                <p className="card-title">{product.name}</p>
+                <p className="card-des">€{product.price.toFixed(2)}</p>
+                <div className="card__btn--container">
+                  <Link to={`/products/${product._id}`}>
+                    <button className="btn__show">Ver detalles</button>
+                  </Link>
+                  <button className="btn__buy" onClick={() => addToCart(product)}>
+                    Añadir al carrito
+                  </button>
+                </div>
+              </div>
             </div>
-            <div className="prod-info">
-              <p className="card-title">{product.name}</p>
-              <p className="card-des">€{product.price.toFixed(2)}</p>
-              <Link to={`/products/${product._id}`}>
-                <button>Ver detalles</button>
-              </Link>
-              <button onClick={() => addToCart(product)}>Añadir al carrito</button>
-            </div>
-          </div>
-        ))}
-      </main>
+          ))}
+        </main>
+      </div>
     </>
   );
 };
